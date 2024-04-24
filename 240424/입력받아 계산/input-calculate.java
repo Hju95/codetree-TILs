c6c189aa53676fdbe621ffc0d0b294c7daddcdbf
Jugwang Hong <remit_@naver.com>
2024-04-24 23:03:15 +0900

    Title: 입력받아 계산, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240424/입력받아 계산/input-calculate.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a = sc.nextInt();
+
+        a += 2;
+
+        System.out.print(a);
+    }
+}
