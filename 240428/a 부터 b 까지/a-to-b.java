93f94618b72a91c3b20cf4e228836c94c9cf115d
Jugwang Hong <remit_@naver.com>
2024-04-28 17:55:46 +0900

    Title: a 부터 b 까지, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240428/a 부터 b 까지/a-to-b.java
+++ 240428/a 부터 b 까지/a-to-b.java
@@ -2,26 +2,18 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        int a, b;
 
-        a = sc.nextInt();
-        b = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        System.out.print(a + " ");
-        for (int i = a; i < b;) {
-            if (i % 2 == 1 && i < b) {
-                i *= 2;
-                if (i <= b) {
-                    System.out.print(i + " ");
-                }
-            }
-            else if (i % 2 == 0 && i < b) {
-                i += 3;
-                if (i <= b) {
-                    System.out.print(i + " ");
-                }
-            }
-        }    
+        for(int i = a; i <= b;) {
+            System.out.print(i +" ");
+            if(i % 2 == 0)
+                i = i + 3;
+            else
+                i = i * 2;
+        }
     }
 }
