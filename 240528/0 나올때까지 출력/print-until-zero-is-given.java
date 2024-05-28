6f3249ba9a0b194e09f224ca34ebc586ea944d07
Jugwang Hong <remit_@naver.com>
2024-05-28 21:52:41 +0900

    Title: 0 나올때까지 출력, Time: 222ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240528/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            int k = sc.nextInt();
+            if (k == 0) {
+                break;
+            }
+
+            System.out.println(k);
+        }
+
+    }
+}
