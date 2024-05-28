21e928bcefb9c1c916f765cfd8486e834879966d
Jugwang Hong <remit_@naver.com>
2024-05-28 21:00:48 +0900

    Title: 등장횟수 확인, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240528/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+
+        for (int i = 0; i < 5; i++) {
+            int k = sc.nextInt();
+            if (k % 2 == 0) {
+                cnt++;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
